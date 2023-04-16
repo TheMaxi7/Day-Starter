b7b16c22dce7f49084f2cd659d3cc92aec4dbbe3
Davide Altamura <davide.altamura97@gmail.com>
2023-04-16 10:12:23 +0200

    Create sheet.py

---
+++ sheet.py
@@ -0,0 +1,23 @@
+import gspread
+
+# Authenticate with Google Sheets API
+sa = gspread.service_account()
+
+
+class Birthdays():
+    def __init__(self) -> None:
+        # Name of the Google Sheets file
+        self.filename = "google sheet file name here"
+        # Open the file and select the worksheet
+        self.spreadsheet = sa.open(self.filename)
+        self.main_worksheet = self.spreadsheet.worksheet("sheet name here")
+
+    def get_name(self, date: str):
+        # Find all cells with the specified date
+        matching_cells = self.main_worksheet.findall(date)
+        if not matching_cells:
+            return None
+        # Retrieve the names in the first column of each matching cell's row
+        names = [self.main_worksheet.cell(cell.row, 1).value for cell in matching_cells]
+        return names
+
